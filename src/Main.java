1e10cd8e823b569064784970934ee26a48aeb766
mast3 <mast3@LAPTOP-OPK7U8L4>
2019-12-10 18:47:32 +0800

    Refinement

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,45 @@ import Util.*;
 import java.util.Calendar;
 
 public class Main {
-    Util.List<Anime> list = new Util.List();
+    static Util.List<Anime> animeList = new Util.List();
     
     public static void main(String[] args){
-        //Testing ground
+        Main main = new Main();
+        main.setData();
+        
     }
     
     // Sets data
     public void setData(){
-        List<Model.Character> cast = new List();
-        cast.add(new Model.Character("Taki", new VoiceActor()));
-        list.add(new Anime("Your Name", new Studio("Shinkai Animation", "Tokyo", Calendar.getInstance()), cast));
+        createAnime();
+    }
+    
+    public void createAnime(){
+        // Get voice actors
+        List<VoiceActor> vaList = createVoiceActors();
+         
+        // Anime : Karakai
+       Anime karakai = new Anime("Karakai Jouzu no Takagi-san");
+       karakai.addCast(new Model.Character("Takagi-san", vaList.getRecord(0)));
        
+       karakai.addCast(new Model.Character("Nishikata", vaList.getRecord(1)));
+       animeList.add(karakai);
+       
+    }
+    
+    public List<VoiceActor> createVoiceActors(){
+        
+        List<VoiceActor> vaList = new List();
+        Quick quick = new Quick();
+        vaList.add(new VoiceActor(quick.generateListId("VA", 5), "Takahashi Rie", Quick.getDate(27, 2, 1994)));
+        vaList.add(new VoiceActor(quick.generateListId("VA", 5), "Yuki Kaji", Quick.getDate(3, 9, 1985)));
+       
+        return vaList;
+    }
+    
+    public void LinkCharactersToActors(){
+        List<Model.Character> cast = new List();
+        
+        
     }
 }
